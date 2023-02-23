af268ae4a2280b488d1646883a6fef7b6add774b
Flores, Alejo Mohamed <floresalejo3@gmail.com>
2023-02-23 19:06:00 -0300

    .
    .

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+const hamburgerIcon = document.querySelector('.nav__hamburguer');
+const navOverlay = document.querySelector('.nav__overlay');
+
+
+hamburgerIcon.addEventListener('click', ()=>{
+    hamburgerIcon.classList.toggle('nav__hamburguer--open');
+
+    navOverlay.classList.toggle('nav__overlay--show');
+
+    document.getElementById('logoAnimado').classList.toggle('floating');
+});
